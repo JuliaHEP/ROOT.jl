d19508cea6d7056178127f5d24f9443634e85bdf
Joosep Pata <joosep.pata@gmail.com>
2014-02-27 18:51:48 +0200

    updated example

--- example/ttree.jl
+++ example/ttree.jl
@@ -1,27 +1,20 @@
-using ROOT
+#file: example/ttree.jl
+#run as: julia example/ttree.jl
 
-#FIXME: currently, default args from Clang are not supported, so they have to be explicitly specified
-#ROOT relies quite heavily on them, so it would be nice to fix
+using ROOT
 
 tf = TFile("test.root", "RECREATE")
 ttree = TTree("my_tree", "My Tree")
 
-#branch variable should be array
+#branch variable should be array with length 1
 x = Float64[0]
-
-#TTree::Branch function expects void*
 px = convert(Ptr{Void}, x)
-
 br = Branch(ttree, "x", px, "x/D")
 
 for i=1:10000
-	x[1] = randn()
-	Fill(ttree)
+    x[1] = randn()
+    Fill(ttree)
 end
 
-branches = GetListOfBranches(ttree)
-for i=1:length(branches)
-	println(root_cast(TBranch, branches[i]))
-end
 Write(tf)
 Close(tf)
