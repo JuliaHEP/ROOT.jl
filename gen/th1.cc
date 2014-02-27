21e6359b40dbbcd977cbf7c7710e33e0041f4deb
Joosep Pata <joosep.pata@gmail.com>
2014-02-27 10:39:14 +0200

    added bin getters

--- gen/th1.cc
+++ gen/th1.cc
@@ -10,8 +10,29 @@ Int_t TH1_Fill2(TH1* __obj, Double_t x, Double_t w) {
 Int_t TH1_Fill3(TH1* __obj, char* name, Double_t w) {
   return __obj->Fill(name, w);
 }
+Double_t TH1_GetBinContent1(TH1* __obj, Int_t bin) {
+  return __obj->GetBinContent(bin);
+}
+Double_t TH1_GetBinContent2(TH1* __obj, Int_t binx, Int_t biny) {
+  return __obj->GetBinContent(binx, biny);
+}
+Double_t TH1_GetBinContent3(TH1* __obj, Int_t binx, Int_t biny, Int_t binz) {
+  return __obj->GetBinContent(binx, biny, binz);
+}
+Double_t TH1_GetBinError1(TH1* __obj, Int_t bin) {
+  return __obj->GetBinError(bin);
+}
+Double_t TH1_GetBinError2(TH1* __obj, Int_t binx, Int_t biny) {
+  return __obj->GetBinError(binx, biny);
+}
+Double_t TH1_GetBinError3(TH1* __obj, Int_t binx, Int_t biny, Int_t binz) {
+  return __obj->GetBinError(binx, biny, binz);
+}
 Double_t TH1_GetEntries1(TH1* __obj ) {
   return __obj->GetEntries();
+}
+Int_t TH1_GetNbinsX1(TH1* __obj ) {
+  return __obj->GetNbinsX();
 }
 Double_t TH1_Integral1(TH1* __obj, Option_t* option) {
   return __obj->Integral(option);
