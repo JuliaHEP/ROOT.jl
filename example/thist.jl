a45fd274b86694126d3b328764ee0c63f5284330
Joosep Pata <joosep.pata@gmail.com>
2014-03-11 08:29:15 +0200

    tests

--- example/thist.jl
+++ example/thist.jl
@@ -1,8 +1,10 @@
 using ROOT
+using Base.Test
 
 hi = TH1D("my_hist", "My Hist", 10, -3.0, 3.0)
 for i=1:1000000
 	Fill(hi, randn())
 end
 
-println("i=", Integral(hi), " e=", GetEntries(hi))
+Integral(hi)::Float64
+@test GetEntries(hi)==1000000
