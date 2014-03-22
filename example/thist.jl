fcb317d3241bc3fc3d1c32efea9969ea2d5a1cc3
Joosep Pata <joosep.pata@gmail.com>
2014-03-22 19:27:41 +0100

    tests are more comprehensive

--- example/thist.jl
+++ example/thist.jl
@@ -1,20 +1,29 @@
 using ROOT
 using Base.Test
 
-println(methods(TH1D))
+#println(methods(TH1D))
 hi = TH1D("my_hist", "My Hist", 10, -3.0, 3.0)
-for i=1:1000000
+@test bytestring(GetName(hi)) == "my_hist"
+@test GetNbinsX(hi) == 10
+
+n=1000000
+for i=1:n
 	Fill(hi, randn())
 end
 
+@test GetEntries(hi) == n
+
 Integral(hi)::Float64
-@test GetEntries(hi)==1000000
 
 bx = Float64[0.0, 1.0, 2.0]
 by = Float64[0.0, 1.0]
 
-println(methods(TH2D))
+#println(methods(TH2D))
 hi2 = TH2D("my_hist2", "My Hist", 2, pointer(bx), 1, pointer(by))::TH2D
+@test bytestring(GetName(hi2))=="my_hist2"
+@test GetNbinsX(hi2) == 2
+@test GetNbinsY(hi2) == 1
 
-Fill(hi2, 0.0, 0.0, 0.0)
-
+Fill(hi2, 0.0, 0.0, 0.5)
+@test GetEntries(hi2) == 1
+@test Integral(hi2) == 0.5
--- example/ttree.jl
+++ example/ttree.jl
@@ -2,20 +2,48 @@
 #run as: julia example/ttree.jl
 
 using ROOT
+using Base.Test
 
 tf = TFile("test.root", "RECREATE")
 ttree = TTree("my_tree", "My Tree")
+@test bytestring(GetName(ttree)) == "my_tree"
 
 #branch variable should be array with length 1
 x = Float64[0]
 px = convert(Ptr{Void}, x)
-println(methods(Branch))
+#println(methods(Branch))
 br = Branch(ttree, "x", px, "x/D")
 
-for i=1:10000
-    x[1] = randn()
+n = 100
+sum_entries = 0.0
+for i=1:n
+    x[1] = i
+    sum_entries += x[1]
     Fill(ttree)
 end
 
+@test GetEntries(ttree) == n
+
 Write(tf)
+Close(tf)
+
+tf = TFile("test.root")
+ttree = Get(tf, "my_tree")
+@test bytestring(ClassName(ttree)) == "TTree"
+
+ttree = root_cast(TTree, ttree)
+
+@test GetEntries(ttree) == n
+
+br = GetBranch(ttree, "x")
+SetAddress(br, convert(Ptr{Void}, x))
+
+sum_entries2 = 0.0
+for i=1:n
+    GetEntry(ttree, i-1)
+    sum_entries2 += x[1]
+end
+
+@test sum_entries == sum_entries2
+
 Close(tf)
