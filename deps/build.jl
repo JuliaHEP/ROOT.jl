64db8512e3ccd5f13be226e4712535e3b4cd7cc1
Joosep Pata <joosep.pata@gmail.com>
2014-04-04 10:42:40 +0300

    build script does cd, renamed test dir

--- deps/build.jl
+++ deps/build.jl
@@ -1,5 +1,9 @@
-using BinDeps 
+using BinDeps
 @BinDeps.setup
+
+haskey(ENV, "ROOTSYS") ||
+    error("could not find ROOT, run `source /path/to/root/bin/thisroot.sh`")
+cd(joinpath(dirname(Base.source_path()), ".."))
 
 @linux_only run(`make`)
 @osx_only run(`make`)
