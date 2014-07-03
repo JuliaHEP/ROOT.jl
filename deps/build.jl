f17a0ca38c4f9e50f1e94912460ea06b212f75b4
Joosep Pata <joosep.pata@cern.ch>
2014-07-03 12:45:37 +0300

    added REQUIRE file

--- deps/build.jl
+++ deps/build.jl
@@ -5,6 +5,6 @@ haskey(ENV, "ROOTSYS") ||
     error("could not find ROOT, run `source /path/to/root/bin/thisroot.sh`")
 cd(joinpath(dirname(Base.source_path()), ".."))
 
-@linux_only run(`make`)
-@osx_only run(`make`)
+@linux_only run(`make lib-linux`)
+@osx_only run(`make lib-osx`)
 @windows_only error("windows not yet supported, tested, try `make`")
