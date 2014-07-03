e6a3fcedca8e5a27939c7e497d9d868d4c1f1876
Joosep Pata <joosep.pata@cern.ch>
2014-07-03 11:59:37 +0300

    names for histograms

--- test/histroot.jl
+++ test/histroot.jl
@@ -7,7 +7,7 @@ h = Histogram([-Inf, 0,1,2,3,4, Inf])
 for i=1:100
     hfill!(h, 0.3)
 end
-th = to_root(h)
+th = to_root(h, "hist1")
 @test from_root(th) == h
 #Print(th, "ALL")
 
@@ -15,7 +15,7 @@ h = Histogram([-Inf, 0,1,2,3,4, Inf])
 for i=1:100
     hfill!(h, 0.3, 0.1)
 end
-th = to_root(h)
+th = to_root(h, "hist2")
 @test from_root(th) == h
 
 
@@ -23,7 +23,7 @@ nh = NHistogram({[-Inf, 0,1,2, Inf], [-Inf, 0,1,2, Inf]})
 for i=1:100
     hfill!(nh, {0.3, 0.3})
 end
-th = to_root(nh)
+th = to_root(nh, "nhist")
 nh2 = from_root(th)
 @test size(nh) == size(nh2)
 @test all(nh2.baseh.bin_entries .== nh.baseh.bin_entries)
