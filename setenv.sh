6525bcb09a226003d0273ca8e6c6f9384dce2121
Joosep Pata <joosep.pata@cern.ch>
2015-09-01 21:27:16 +0300

    ls->find to not have colour errors

--- setenv.sh
+++ setenv.sh
@@ -1,5 +1,6 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-export JULIA_LIB=`ls $MY_JULIA_HOME/usr/lib/libjulia* | head -n1`
-export JULIAREPL_LIB=`ls $DIR/librepl* | head -n1`
-export DYLD_LIBRARY_PATH=$MY_JULIA_HOME/usr/lib/julia/:$DYLD_LIBRARY_PATH
+export JULIA_LIB=`find $MY_JULIA_HOME/usr/lib -name "libjulia*" | head -n1`
+export JULIAREPL_LIB=`find $DIR -name librepl | head -n1`
+export LD_LIBRARY_PATH=$MY_JULIA_HOME/usr/lib/julia/:$LD_LIBRARY_PATH #on linux
+export DYLD_LIBRARY_PATH=$MY_JULIA_HOME/usr/lib/julia/:$DYLD_LIBRARY_PATH #on osx
 export PATH=$DIR:$PATH
