b33347dd7bffd969cbc73ba48d1bd4ae451255ca
Joosep Pata <joosep.pata@gmail.com>
2016-07-25 10:21:10 +0200

    updated ui

--- src/ui.cc
+++ src/ui.cc
@@ -1,6 +1,8 @@
 #include <dlfcn.h>
 #include <stdio.h>
 #include <iostream>
+#include <julia.h>
+#include <uv.h>
 #include "TROOT.h"
 
 void init_root();
@@ -9,20 +11,40 @@ int main(int argc, char* argv[]) {
   gROOT->ProcessLine("int __dummy__=1;");
   gROOT->ProcessLine("std::cout << \"ROOT: ROOT initialized\" << std::endl;");
 
-  void* handle_julia = dlopen(LIBJULIAREPL, RTLD_NOW | RTLD_GLOBAL);
+  void* handle_julia = dlopen(LIBJULIA, RTLD_NOW | RTLD_GLOBAL);
   if (!handle_julia) {
     fputs(dlerror(), stderr);
     std::cerr << std::endl;
-    std::cerr << "could not load library: " << LIBJULIAREPL << std::endl;
+    std::cerr << "could not load library: " << LIBJULIA << std::endl;
     return 1;
   }
-  typedef int (*t_jl_main)(int, char**);
-  t_jl_main jl_main = (t_jl_main)dlsym(handle_julia, "main");
-  if (!jl_main) {
-    fputs(dlerror(), stderr);
-    std::cerr << std::endl;
-    std::cerr << "could not load main from librepl" << std::endl;
-    return 1;
+  typedef int (*t_jl_init)(const char *);
+  typedef int (*t_jl_set_ARGS)(int, char**);
+  typedef int (*t_jl_eval_string)(const char*);
+  typedef int (*t_jl_atexit_hook)(int);
+  typedef int (*t_jl_load)(const char*);
+  typedef int (*t_jl_parse_opts)(int *argcp, char ***argvp);
+  t_jl_init jl_init = (t_jl_init)dlsym(handle_julia, "jl_init");
+  t_jl_set_ARGS jl_set_ARGS = (t_jl_set_ARGS)dlsym(handle_julia, "jl_set_ARGS");
+  t_jl_eval_string jl_eval_string = (t_jl_eval_string)dlsym(handle_julia, "jl_eval_string");
+  t_jl_atexit_hook jl_atexit_hook = (t_jl_atexit_hook)dlsym(handle_julia, "jl_atexit_hook");
+  t_jl_load jl_load = (t_jl_load)dlsym(handle_julia, "jl_load");
+  t_jl_parse_opts jl_parse_opts = (t_jl_parse_opts)dlsym(handle_julia, "jl_parse_opts");
+  
+  uv_setup_args(argc, argv);
+  jl_init(NULL);
+  jl_parse_opts(&argc, (char***)&argv);
+  jl_set_ARGS(argc, argv);
+  int ret = 0;
+  
+  if (argc > 0) {
+      if (strcmp(argv[0], "-")) {
+          ret = jl_load(argv[0]);
+      }
+  } else {
+    jl_eval_string("Base._start()");
   }
-  return jl_main(argc, argv);
+
+  jl_atexit_hook(0);
+  return ret;
 }
