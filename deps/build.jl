1c21d37a76fe64adc2a27bf59525bd4497739424
Oliver Schulz <oschulz@mpp.mpg.de>
2017-06-02 14:44:06 +0200

    Link root-compatible julia executable with rpath for ROOT and Julia libs

--- deps/build.jl
+++ deps/build.jl
@@ -35,20 +35,24 @@ const INCDIR_SUPPORT_H = dirname(find_file(JULIA_BASE_PATH, "libsupport.h"))
 
 const LIBJULIA_PATH = find_file(JULIA_BASE_PATH, "libjulia.$SHEXT")
 isfile(LIBJULIA_PATH) || error("Could not find libjulia at $LIBJULIA_PATH")
+JULIA_LIBDIR = dirname(LIBJULIA_PATH)
+
+const ROOT_INCDIR = strip(readstring(`root-config --incdir`))
+const ROOT_LIBDIR = strip(readstring(`root-config --libdir`))
 
 @show INCDIR_JULIA_H 
 @show INCDIR_UV_H
 @show INCDIR_SUPPORT_H
 @show LIBJULIA_PATH
+@show JULIA_LIBDIR
+@show ROOT_INCDIR
+@show ROOT_LIBDIR
 
 info("compiling ROOT-compatible julia executable")
-readstring(`make OS=$OS SHLIB_EXT=$SHEXT LIBJULIA_PATH=$LIBJULIA_PATH INCDIR_JULIA_H=$INCDIR_JULIA_H INCDIR_UV_H=$INCDIR_UV_H INCDIR_SUPPORT_H=$INCDIR_SUPPORT_H`) |> println
+readstring(`make OS=$OS SHLIB_EXT=$SHEXT ROOT_LIBDIR=$ROOT_LIBDIR JULIA_LIBDIR=$JULIA_LIBDIR LIBJULIA_PATH=$LIBJULIA_PATH INCDIR_JULIA_H=$INCDIR_JULIA_H INCDIR_UV_H=$INCDIR_UV_H INCDIR_SUPPORT_H=$INCDIR_SUPPORT_H`) |> println
 
 exepath = joinpath(bindir, "julia")
 isfile(exepath) || error("could not find final exe: $exepath, something went wrong in the compilation")
-
-const ROOT_INCDIR = strip(readstring(`root-config --incdir`))
-const ROOT_LIBDIR = strip(readstring(`root-config --libdir`))
 
 depsfile_location = joinpath(splitdir(Base.source_path())[1],"deps.jl")
 
