410e7d42bc491af87f52b5ec1efd863587b01efa
Tony Kelman <tony@kelman.net>
2017-06-10 11:08:50 -0700

    use mktempdir() do block
    this will clean up the temporary directory when finished

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,48 +13,50 @@ cxxinclude("TH1D.h")
 
 
 @Base.Test.testset "Package ROOT" begin
-    scratchdir = mktempdir()
+    mktempdir() do scratchdir
 
-    @testset "TTree and TChain" begin
+        @testset "TTree and TChain" begin
 
-        cd(scratchdir) do
-            @test icxx"""
-                TFile output_file("testdata.root", "recreate");
-                auto tree = new TTree("data", "Data");
-                Int_t x = 0;
-                tree->Branch("x", &x);
-                for (size_t i = 1; i <= 10; ++i) {
-                    x = 2 * i;
-                    tree->Fill();
-                }
-                output_file.Write();
-                output_file.Close();
-                true;
-            """
+            cd(scratchdir) do
+                @test icxx"""
+                    TFile output_file("testdata.root", "recreate");
+                    auto tree = new TTree("data", "Data");
+                    Int_t x = 0;
+                    tree->Branch("x", &x);
+                    for (size_t i = 1; i <= 10; ++i) {
+                        x = 2 * i;
+                        tree->Fill();
+                    }
+                    output_file.Write();
+                    output_file.Close();
+                    true;
+                """
 
-            @test icxx"""
-                TChain chain("data");
-                chain.AddFile("testdata.root");
-                Int_t x = 0;
-                chain.SetBranchAddress("x", &x);
-                auto n = chain.GetEntries();
-                Int_t s = 0;
-                for (size_t i = 0; i < n; ++i) {
-                    chain.GetEntry(i);
-                    s += x;
-                }
-                s;
-            """ == 110
+                @test icxx"""
+                    TChain chain("data");
+                    chain.AddFile("testdata.root");
+                    Int_t x = 0;
+                    chain.SetBranchAddress("x", &x);
+                    auto n = chain.GetEntries();
+                    Int_t s = 0;
+                    for (size_t i = 0; i < n; ++i) {
+                        chain.GetEntry(i);
+                        s += x;
+                    }
+                    s;
+                """ == 110
+            end
         end
-    end
 
-    @testset "TH1" begin
-        @test icxx"""
-            auto *hist = new TH1D("hist", "Hist", 10, 0, 10);
-            for (Int_t i = 0; i < 10; ++i) hist->Fill(i);
-            auto s = hist->Integral();
-            delete hist;
-            s;
-        """ ≈ 10
-    end
+        @testset "TH1" begin
+            @test icxx"""
+                auto *hist = new TH1D("hist", "Hist", 10, 0, 10);
+                for (Int_t i = 0; i < 10; ++i) hist->Fill(i);
+                auto s = hist->Integral();
+                delete hist;
+                s;
+            """ ≈ 10
+        end
+
+    end # mktempdir
 end
