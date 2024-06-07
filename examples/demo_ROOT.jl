4a02fa3f775e23dbff5511fbec164c1cee886ce1
Pere Mato <pere.mato@cern.ch>
2024-06-07 12:02:48 +0200

    Fix closures on ARM. Cosmetic/usability  changes

--- examples/demo_ROOT.jl
+++ examples/demo_ROOT.jl
@@ -2,24 +2,23 @@
 # The iROOT module is required for interactive graphic display
 # Loading the package trigger a loop that process ROOT graphic events.
 using ROOT
-const R = ROOT #a shortcut
 
 # Create a ROOT histogram, fill it with random events, and fit it.
-h = R.TH1D("h", "Normal distribution", 100, -5., 5.)
-R.FillRandom(h, "gaus")
+h = ROOT.TH1D("h", "Normal distribution", 100, -5., 5.)
+FillRandom(h, "gaus")
 
 #Draw the histogram on screen
-c = R.TCanvas()
-R.Draw(h)
+c = ROOT.TCanvas()
+Draw(h)
 
 #Fit the histogram wih a normal distribution
-R.Fit(h, "gaus")
+Fit(h, "gaus")
 
 #Save the Canvas in an image file
-R.SaveAs(c, "demo_ROOT.png")
+SaveAs(c, "demo_ROOT.png")
 
 #Save the histogram and the graphic canvas in the demo_ROOT_out.root file.
-f = R.TFile!Open("demo_ROOT_out.root", "RECREATE")
-R.Write(h)
-R.Write(c)
+f = ROOT.TFile!Open("demo_ROOT_out.root", "RECREATE")
+Write(h)
+Write(c)
 Close(f)
--- examples/demo_fit_with_jl_func_2.jl
+++ examples/demo_fit_with_jl_func_2.jl
@@ -1,19 +1,20 @@
 using ROOT
-const R = ROOT # A shortcut
 
 # Defines a TGraph with the points to fit
-x = collect(1.:10.); y = map(x -> x^2 * (1 + 0.1*randn()), x)
-g = TGraphW(convert(Int32, length(x)), x, y)
+x = range(1, 10, length=20)
+y = map(x -> x^2 * (1 + 0.1*randn()), x);
+
+g = ROOT.TGraph(x, y)
 
 # Function for the fit
 f_jl(x,y) = x[1]^y[1]
 
 println("\nFit a TGraph with a function defined in Julia.")
-R.Fit(f_jl, g, "", "", 0., 10., 1, 1, 0)
+Fit(f_jl, g, "", "", 0., 10., 1, 1, 0)
 
 #   embedded in ROOT TF1
 println("Define a TF1 from a Julia function")
-tf1 = R.TF1(f_jl, "func", 0., 10., 1, 1, 0)
+tf1 = ROOT.TF1(f_jl, "func", 0., 10., 1, 1, 0)
 
 println("\nRedo the fit using the TF1 function refered by its name.")
 Fit(g, "func")
@@ -21,8 +22,8 @@ Fit(g, "func")
 println("\nRedo the fit using the TF1 function refered by its instance.")
 Fit(g, tf1)
 
-c = R.TCanvas()
+c = ROOT.TCanvas()
 Draw(g, "A*")
 
 #Save the Canvas in an image file
-R.SaveAs(c, "demo_fit_with_jl_func_2.png")
+SaveAs(c, "demo_fit_with_jl_func_2.png")
